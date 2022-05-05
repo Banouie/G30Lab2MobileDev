bc300513b7db665fc5ebe3b0e5a468c1aa335176
Guido Genco <50440306+Guido27@users.noreply.github.com>
2022-05-05 17:55:01 +0200

    Implemented TimePicker

--- app/src/main/java/com/g30lab3/app/ui/timeSlotEdit/TimeSlotEditFragment.kt
+++ app/src/main/java/com/g30lab3/app/ui/timeSlotEdit/TimeSlotEditFragment.kt
@@ -6,67 +6,68 @@ import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.g30lab3.app.R
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.time.Duration.Companion.days
 
 
 class TimeSlotEditFragment : Fragment(R.layout.fragment_time_slot_edit) {
 
 
-    lateinit var btnSelectDate:EditText
+    lateinit var dateSelector: EditText
+    lateinit var timeSelector: EditText
+
+    var time: String=""
+    var date: String=""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    /*
         super.onViewCreated(view, savedInstanceState)
 
+        dateSelector = view.findViewById(R.id.edit_date)
+        timeSelector = view.findViewById(R.id.edit_time)
 
-        val DateChooser = view.findViewById<EditText>(R.id.edit_date_Field)
-        DateChooser.setOnClickListener{
-            val datePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select date")
-                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+        // *** time picker relative code ***
+        val timePicker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(12)
+                .setMinute(10)
                 .build()
-            datePicker.show(childFragmentManager,null)
+        // show it
+        timeSelector.setOnClickListener {
+            timePicker.show(parentFragmentManager, "tag");
+        }
+        // get positive time selection
+        timePicker.addOnPositiveButtonClickListener {
+            time = timePicker.hour.toString() + " : " + timePicker.minute.toString()
+            timeSelector.setText(time)
         }
 
-         */
-
-        super.onViewCreated(view, savedInstanceState)
-        btnSelectDate = view.findViewById(R.id.edit_date)
-
-        btnSelectDate.setOnClickListener(View.OnClickListener {
-            val newFragment: DialogFragment = DatePickerFragment()
-            fragmentManager?.let { it1 -> newFragment.show(it1, "DatePicker") }
-        })
-
-
-
-        btnSelectDate =  view.findViewById(R.id.edit_date)
-        btnSelectDate.inputType = 0;
-
-        view.apply {
-
-            btnSelectDate.setOnClickListener {
-                // create new instance of DatePickerFragment
-
-                val datePickerFragment = DatePickerFragment()
-                val supportFragmentManager = requireActivity().supportFragmentManager
-
-                // we have to implement setFragmentResultListener
-                supportFragmentManager.setFragmentResultListener(
-                    "REQUEST_KEY",
-                    viewLifecycleOwner
-                ) { resultKey, bundle ->
-                    if (resultKey == "REQUEST_KEY") {
-                        val date = bundle.getString("SELECTED_DATE")
-                        btnSelectDate.setText(date)
-                    }
-                }
-                // show
-                datePickerFragment.show(supportFragmentManager, "DatePickerFragment")
-            }
+        // *** date picker relative code ***
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build()
+        // show it
+        dateSelector.setOnClickListener {
+            datePicker.show(parentFragmentManager, "tag");
         }
+        // get positive date selection
+        datePicker.addOnPositiveButtonClickListener {
+            val utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            utc.timeInMillis = it
+            val format = SimpleDateFormat("dd-MM-yyyy")
+            date = format.format(utc.time)
+            dateSelector.setText(date)
+        }
+
+
+
 
         //TODO manage back button pression for saving edited timeslot
-
 
 
     }
