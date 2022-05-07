43aca142041bf504388c26ca57614cb86078068f
Guido Genco <50440306+Guido27@users.noreply.github.com>
2022-05-07 18:28:56 +0200

    Added messsage of no available timeSlots in the home if there are no timeSlots

--- app/src/main/java/com/g30lab3/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/g30lab3/app/ui/home/HomeFragment.kt
@@ -1,8 +1,8 @@
 package com.g30lab3.app.ui.home
+
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.*
@@ -11,11 +11,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.g30lab3.app.R
 import com.g30lab3.app.adaptors.TimeSlotAdapter
-import com.g30lab3.app.databinding.FragmentHomeBinding
-import com.g30lab3.app.models.timeSlot
 import com.g30lab3.app.timeSlotVM
-import com.g30lab3.app.ui.home.HomeViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -24,18 +22,27 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView:RecyclerView = view.findViewById(R.id.rv)
+        var emptyMessage: TextView = view.findViewById(R.id.empty_message)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.rv)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        vm.all.observe(requireActivity()){
+        vm.all.observe(requireActivity()) {
+
             // Data bind the recycler view
             recyclerView.adapter = TimeSlotAdapter(it)
+
+            //if the list of timeSlot is empty a message is shown, shouldn't appear otherwise
+            if (it.isEmpty()) {
+                emptyMessage.visibility = View.VISIBLE
+            }else{
+                emptyMessage.visibility = View.GONE
+            }
         }
 
         var addButton: FloatingActionButton = view.findViewById(R.id.floating_add_button)
-        addButton.setOnClickListener{
+        addButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_nav_timeSlotEditFragment)
         }
-
 
 
     }
