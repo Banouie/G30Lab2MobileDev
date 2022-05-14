881b0b826cb49db92f2daa3d213b59471c62ac98
Guido Genco <50440306+Guido27@users.noreply.github.com>
2022-05-14 19:33:13 +0200

    Implemented login layout, need to implement the login logic

---
+++ app/src/main/java/com/g30lab3/app/ui/Login/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.g30lab3.app.ui.Login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.g30lab3.app.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
