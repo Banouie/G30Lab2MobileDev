cc338feaea109844ae92cca00c54f381d59ce70e
Guido Genco <50440306+Guido27@users.noreply.github.com>
2022-05-27 22:10:36 +0200

    Added send request dialog in chat

--- app/src/main/java/com/g30lab3/app/ui/chat/chatFragment.kt
+++ app/src/main/java/com/g30lab3/app/ui/chat/chatFragment.kt
@@ -2,9 +2,13 @@ package com.g30lab3.app.ui.chat
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,9 +19,11 @@ import com.g30lab3.app.UserVM
 import com.g30lab3.app.adapters.MessagesAdapter
 import com.g30lab3.app.chatsVM
 import com.g30lab3.app.models.textMessage
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.delay
 import java.util.*
 
 
@@ -26,34 +32,52 @@ class chatFragment : Fragment(R.layout.fragment_chat) {
     val chatVM by viewModels<chatsVM>()
 
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView :RecyclerView = view.findViewById(R.id.chat)
+        val recyclerView: RecyclerView = view.findViewById(R.id.chat)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val timeSlotId =  arguments?.get("timeSlotId") as String
+        val timeSlotId = arguments?.get("timeSlotId") as String
         val authorUserId = arguments?.get("authorUser") as String
         val requestUserId = arguments?.get("requestUser") as String
-        val chatId = requestUserId+authorUserId+timeSlotId //obtain the unique Id for the chat
+        val chatId = requestUserId + authorUserId + timeSlotId //obtain the unique Id for the chat
 
-        chatVM.getChat(chatId,requestUserId,authorUserId,timeSlotId)//get the chat from VM
-        chatVM.currentChat.observe(requireActivity()){
+        chatVM.getChat(chatId, requestUserId, authorUserId, timeSlotId)//get the chat from VM
+        chatVM.currentChat.observe(requireActivity()) {
             recyclerView.adapter = MessagesAdapter(it)
         }
 
-        var sender:TextInputLayout = view.findViewById(R.id.insert_message)
-        sender.setEndIconOnClickListener{
-            val messageText = sender.editText?.text.toString()
-            val senderId = Firebase.auth.currentUser?.uid!!
-            val now = Date()
-            val message = textMessage(messageText,now,senderId)
-            chatVM.addMessage(chatId,message)
-            sender.editText?.text?.clear()
+        var sender: TextInputLayout = view.findViewById(R.id.insert_message)
+        sender.helperText = HtmlCompat.fromHtml(
+            "<b>Tap</b>: send a message, <b>long press</b>: send a request",
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+            sender.setEndIconOnLongClickListener {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Send a request")
+                    .setMessage("Are you shure you want to request this Time Slot?")
+                    .setNegativeButton("Discard") { dialog, which ->
+                        // Respond to negative button press
+                        Log.d("BTNPRESSED", "Discard selected")
+                    }
+                    .setPositiveButton("Yes I'm sure") { dialog, which ->
+                        Log.d("BTNPRESSED", "Yes selected")
+                    }
+                    .show()
+                true
+            }
+            sender.setEndIconOnClickListener {
+                val messageText = sender.editText?.text.toString()
+                val senderId = Firebase.auth.currentUser?.uid!!
+                val now = Date()
+                val message = textMessage(messageText, now, senderId)
+                chatVM.addMessage(chatId, message)
+                sender.editText?.text?.clear()
+            }
         }
+
 
     }
 
 
-
-}
+        //TODO edit chat fragment label
